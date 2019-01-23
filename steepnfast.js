b4fccff9c34c2d36de07c82823d431f2602fd370
alexmillerpcc <alex.miller1pcc.edu>
2019-01-23 14:56:00 -0800

    first commit

---
+++ steepnfast.js
@@ -0,0 +1,41 @@
+
+function setup(){
+	createCanvas(400,400);
+
+}
+
+function draw(){
+	background(200);
+
+	fill(0);
+
+	beginShape();
+	vertex(200, 200);
+	vertex(230, 270);
+	vertex(215, 270);
+	vertex(200, 235);
+	vertex(185, 270);
+	vertex(170, 270);
+	endShape();
+
+	beginShape();
+	vertex(190, 100);
+	vertex(270, 270);
+	vertex(240, 270);
+	vertex(190, 150);
+	vertex(140, 270);
+	vertex(110, 270);
+	endShape();
+
+	beginShape();
+	vertex(220, 140);
+	vertex(230, 140);
+	vertex(270, 220);
+	vertex(270, 240);
+	endShape();
+
+
+
+
+
+}
